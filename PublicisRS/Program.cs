0473ca31cca09ca8dc28386b9c64f95393028cd9
YOGESH <YOGESH@DESKTOP-CBG36P7>
2024-03-14 20:00:42 +0530

    Print the Number and string

--- PublicisRS/Program.cs
+++ PublicisRS/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PublicisRS;
+
+PrintNameOrNumber objPrintNameorNumber = new PrintNameOrNumber();
+
+for (int i = 1; i < 100; i++)
+{
+    if (objPrintNameorNumber.PublicisrOar(i) == "PublicisrOar")
+    {
+        Console.WriteLine(objPrintNameorNumber.PublicisrOar(i));
+    }
+    else if (objPrintNameorNumber.Publicis(i) == "Publicis")
+    {
+        Console.WriteLine(objPrintNameorNumber.Publicis(i));
+    }
+    else if (objPrintNameorNumber.ROar(i) == "rOar")
+    {
+        Console.WriteLine(objPrintNameorNumber.ROar(i));
+    }
+    else
+    {
+        Console.WriteLine(objPrintNameorNumber.PrintNumber(i));
+    }
+}
+
+Console.ReadKey();
+
