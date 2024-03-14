9e57d2664ab85e7030e3f45559aaa10df4aed4b5
YOGESH <YOGESH@DESKTOP-CBG36P7>
2024-03-14 19:35:56 +0530

    Test With Fail

---
+++ PublicisRS/PrintNameOrNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicisRS
+{
+    public  class PrintNameOrNumber
+    {
+        public string Publicis(int number)
+        {
+            return "";
+        }
+        public string ROar(int number)
+        {
+            return "";
+        }
+        public string PublicisrOar(int number)
+        {
+            return "";
+        }
+        public string PrintNumber(int number)
+        {
+            return "";
+        }
+    }
+}
